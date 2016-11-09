f79dbda2da211cbc31468f7701b76bee4ee512bf
Christine Sizemore <christine.e.sizemore@gmail.com>
2016-11-08 18:29:33 -0600

    complete assignment

---
+++ assets/javascript/app.js
@@ -0,0 +1,89 @@
+$(function(){
+
+	/* Instantiate global vars */
+	var topics = [
+		'Steven Universe',
+		'Gravity Falls',
+		'The Regular Show',
+		'TMNT',
+		'Futurama',
+		'Adventure Time',
+		'Bravest Warriors',
+		'Bee and Puppycat',
+		'Avatar the Last Airbender',
+		'Avatar the Legend of Korra',
+		'Fullmetal Alchemist'
+	]
+
+	/* Create buttons */
+	$.each(topics, function(){
+		appendBtn(this);
+	});
+
+
+	 function appendBtn(btn){
+		$('#gif-select').append(
+			'<button type="button" class="btn btn-default topic">' +
+				btn +
+			'</button>'
+		);
+	}
+
+	/* Set click listener for topic */
+	$('body').on('click', '.topic', function(){
+		// Clear any existing gifs
+		$('#gif-display').html('');
+
+		// Fetch the topic
+		var topic = $(this).html();
+
+		/* Ping GIPHY API with topic keyword */
+		$.ajax({
+			url: 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=' + topic,
+		}).done(function(gifset) {
+			// Cycle through response data
+			$.each(gifset.data, function(){
+				// Get the still and dynamic URLs
+				var dynamic = this.images.original.url;
+				var static = this.images.original_still.url;
+				// Display gif w data attributes for image states
+				$('#gif-display').append(
+					'<img' +
+					' class="gif"' +
+					' data-static="' + static + '"' +
+					' data-dynamic="' + dynamic + '"' +
+					' data-animate="false"' +
+					' src="' + static + '"' +
+					' />'
+				);
+			});
+		});
+	});
+
+	/* Set click listener for gif */
+	$('body').on('click', '.gif', function(){
+		// Find image state & replace the img src accordingly
+		var isAnimated = $(this).data('animate');
+		var staticUrl = $(this).data('static');
+		var dynamicUrl = $(this).data('dynamic');
+
+		if(isAnimated){
+			// Stop if started
+			$(this).data('animate', false).attr('src', staticUrl);
+		}else{
+			// Start if stopped
+			$(this).data('animate', true).attr('src', dynamicUrl);
+		}
+	});
+
+	$('#submit-toon').on('click', function(e){
+		// Don't do get or post
+		e.preventDefault();
+		// Add button for the new topic
+		var toon = $('#my-toon').val();
+		appendBtn(toon);
+		// Clear the input
+		$('#my-toon').val('');
+	});
+
+});
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>GifTastic</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+	<header class="site-header">
+		<div class="container">
+			<h1 class="page-title">GifTastic</h1>
+		</div>
+	</header>
+	<section id="gif-select" class="section container">
+	</section>
+	<section id="gif-add" class="section container">
+		<form>
+			<label>Add your favorite cartoon <input id="my-toon" type="text" /></label>
+			<input id="submit-toon" type="submit" />
+		</form>
+	</section>
+	<section id="gif-display" class="section container">
+	</section>
+
+	<!-- Call jQuery and game JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/javascript/app.js"></script>
+
+	<!-- Latest compiled and minified JavaScript for Bootstrap -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
